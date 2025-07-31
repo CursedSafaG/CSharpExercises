b5158170a12857f069868bfc543d512637ea7142
Safa Gökalp Uğur <ugursafagokalp@gmail.com>
2025-07-31 11:49:33 +0300

    First two classes

---
+++ 02C#_Variables/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02C__Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Variable
+
+            //double number;
+
+            //number = 4.85;
+
+            //Console.WriteLine(number);
+
+            //double applePrice, bananaPrice, potatoPrice, tomatoPrice;
+            //applePrice = 10.50;
+            //bananaPrice = 30.75;
+            //potatoPrice = 20.10;
+            //tomatoPrice = 13.85;
+
+            //Console.WriteLine("**** Prices ****");
+            //Console.WriteLine();
+            //Console.WriteLine("---- Apple Price : " + applePrice + "TL");
+            //Console.WriteLine("---- Banana Price : " + bananaPrice + "TL");
+            //Console.WriteLine("---- Potato Price : " + potatoPrice + "TL");
+            //Console.WriteLine("---- Tomato Price : " + tomatoPrice + "TL");
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------------------");
+            //double appleCount, bananaCount, potatoCount, tomatoCount, totalPrice;
+
+            //appleCount = 4;
+            //bananaCount = 5.5;
+            //potatoCount = 1.3;
+            //tomatoCount = 0.4;
+            //totalPrice = appleCount*applePrice + bananaCount*bananaPrice + potatoCount*potatoPrice + tomatoCount*tomatoPrice;
+            //Console.WriteLine();
+            //Console.WriteLine("Total Price is : "+totalPrice+"TL");
+
+            #endregion
+
+            #region Char Variables
+
+            //      'a'
+
+            //char symbol;
+
+            //symbol = 'a';
+
+            //Console.WriteLine(symbol);
+            #endregion
+
+            #region Input From Keyboard(String)
+
+            //Console.WriteLine("***** C# Airlines *****");
+            //Console.WriteLine();
+
+            //string passangerName, passangerSurname, passangerDestination, passangerCity, passangerAge, passangerID;
+
+            //Console.Write("Passanger Name : ");
+            //passangerName = Console.ReadLine();
+
+            //Console.Write("Passanger Surname : ");
+            //passangerSurname = Console.ReadLine();
+
+            //Console.Write("Passanger Destination : ");
+            //passangerDestination = Console.ReadLine();
+
+            //Console.Write("Passanger will fly from : ");
+            //passangerCity = Console.ReadLine();
+
+            //Console.Write("Passanger Age : ");
+            //passangerAge = Console.ReadLine();
+
+            //Console.Write("Passanger ID : ");
+            //passangerID = Console.ReadLine();
+
+            //Console.WriteLine("----------------------------");
+
+            //Console.WriteLine("Passanger : " + passangerName + " " + passangerSurname);
+
+            //Console.WriteLine("Pasanger is flying from " + passangerCity + " To " + passangerDestination);
+
+            //Console.WriteLine("Passanger Age : " + passangerAge + " Passanger ID : " + passangerID);
+
+            #endregion
+
+            #region Input From Keyboard(Integer)
+
+            //int shoePrice, computerPrice, chairPrice, mousePrice, phonePrice;
+
+            //shoePrice = 1000;
+            //computerPrice = 50000;
+            //chairPrice = 550;
+            //mousePrice = 195;
+            //phonePrice = 11200;
+
+            //int shoeCount, computerCount, chairCount, mouseCount, phoneCount;
+
+            //Console.Write("Shoes Count : ");
+            //shoeCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Computer count : ");
+            //computerCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Chair count : ");
+            //chairCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Mouse count : ");
+            //mouseCount = int.Parse(Console.ReadLine());
+
+            //Console.Write("Phone count : ");
+            //phoneCount = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("---------------------------------");
+
+            //int totalPrice;
+
+            //totalPrice = shoeCount * shoePrice + computerCount * computerPrice + chairCount * chairPrice + mouseCount * mousePrice + phonePrice * phoneCount;
+
+            //Console.WriteLine("Total price : " + totalPrice);
+
+            #endregion
+
+            #region Input From Keyboard(Double)
+
+            //double exam1, exam2, exam3 , result;
+            //Console.Write("Please enter 1st exam point : ");
+            //exam1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter 2nd exam point : ");
+            //exam2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Please enter 3rd exam point : ");
+            //exam3 = double.Parse(Console.ReadLine());
+
+            //result = (exam1 + exam2 + exam3) / 3;
+
+            //Console.WriteLine();
+            //Console.Write("Result : " + result);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
